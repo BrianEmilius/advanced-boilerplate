390b1791737273a17cbd733c084d12ca1e2f1d67
Brian Emilius <BrianEmilius@users.noreply.github.com>
2017-06-06 10:51:03 +0200

    routes reorganization

--- app/routes/backend/pages.js
+++ app/routes/backend/pages.js
@@ -1,14 +1,14 @@
 const Pages = require('../../controllers/page');
 
 module.exports = function (app) {
-	app.get('/admin/pages', function (req, res) {
+	app.get('/dash/pages', function (req, res) {
 		let pages = new Pages();
 		pages.retrieveList(function (result) {
 			res.render('backend/pageList.pug', { title: res.__('Pages'), tagline: '', pages: result });
 		});
 	});
 
-	app.get('/admin/page/:id', function (req, res) {
+	app.get('/dash/page/:id', function (req, res) {
 		let pages = new Pages();
 		pages.retrieveOne(req.params.id, function (result) {
 			res.render('backend/pageEdit.pug', { title: res.__('Edit Page'), tagline: '', page: result });
